13492b0de504d54c8ca756770d4fd52bdd0f8df8
= <dmastergame74@gmail.com>
2025-03-27 20:14:58 -0300

    create usage for websocket server

--- src/main/java/dev/buskopan/Main.java
+++ src/main/java/dev/buskopan/Main.java
@@ -1,7 +1,13 @@
 package dev.buskopan;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ServerWebSocket.start(8080);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
